43d396fa005c825eefe62ea87b55d98097dee770
grace12021 <grace12021@gmail.com>
2021-12-01 14:36:24 -0800

    Update Day01.kt
    Finish Day1 of the challenge.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val length = input.size
+
+        var count = 0
+
+        for (i in 0..length-2)
+            if (input[i].toInt() < input[i+1].toInt()) count+=1
+
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val length = input.size
+
+        var count = 0
+
+        for (i in 0..length-4)
+            if (input[i].toInt() < input[i+3].toInt()) count+=1
+
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
